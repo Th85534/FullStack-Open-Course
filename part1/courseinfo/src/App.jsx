fcb3fbf3cf881b45bf1947a3bb6c474186a0f4ce
Soumasish Dasgupta <72874054+Th85534@users.noreply.github.com>
2025-02-11 17:22:59 +0000

    modified:   part1/courseinfo/src/App.jsx 	new file:   part1/unicafe/.gitignore 	new file:   part1/unicafe/README.md 	new file:   part1/unicafe/eslint.config.js 	new file:   part1/unicafe/index.html 	new file:   part1/unicafe/package-lock.json 	new file:   part1/unicafe/package.json 	new file:   part1/unicafe/public/vite.svg 	new file:   part1/unicafe/src/App.jsx     new file:   part1/unicafe/src/main.jsx 	new file:   part1/unicafe/vite.config.js

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,8 +5,8 @@ const Header = ({ course }) => {
 const Content = ({ parts }) => {
   return (
     <div>
-      {parts.map((part) => (
-        <p key={part.id}>
+      {parts.map((part, index) => (
+        <p key={index}>
           {part.name} {part.exercises}
         </p>
       ))}
@@ -20,18 +20,20 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    { id: 1, name: "Fundamentals of React", exercises: 10 },
-    { id: 2, name: "Using props to pass data", exercises: 7 },
-    { id: 3, name: "State of a component", exercises: 14 }
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 }
+    ]
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
