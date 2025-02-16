374348d4f50654292abbf9b696e2c6b356943515
Th85534 <dasguptasoumasish@gmail.com>
2025-02-16 04:37:38 +0000

    modified:   part2/phonebook/src/services/phonebook.js

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = 'https://fullstack-open-course-6cw3.onrender.com/persons';
 
 const getAll = () => axios.get(baseUrl).then(response => response.data);
 
