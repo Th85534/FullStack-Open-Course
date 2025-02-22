929ccbb9d51f8704ed1aad62ea3fc53b16cf0c7b
Th85534 <dasguptasoumasish@gmail.com>
2025-02-22 12:08:33 +0000

    modified:   part4/blog-list/app.js 	modified:   part4/blog-list/controllers/blogs.js 	new file:   part4/blog-list/controllers/login.js 	modified:   part4/blog-list/utils/middleware.js

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config');
 const logger = require('./utils/logger');
 const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
+const loginRouter = require('./controllers/login');
 
 mongoose.set('strictQuery', false)
 logger.info('connecting to MongoDB')
@@ -23,6 +24,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,7 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
@@ -13,8 +21,12 @@ blogsRouter.post('/', async (request, response) => {
   if (!title || !url) {
     return response.status(400).json({ error: 'Title and URL are required' })
   }
-  // Find any user to assign as the creator
-  const user = await User.findOne()
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
     return response.status(400).json({ error: 'No users found in the database' })
